349c1fd7c22e34651ed70612ed1f5de58b718b56
Taeho <rjsdnxogh55@gmail.com>
2024-05-04 21:42:10 +0900

    Fix: Change Package Test Code
    - 테스트 코드도 PnuBookStore로 패키지를 변경시켜줬습니다.

--- pnuBookStore/src/test/java/org/example/pnubookstore/PnuBookStoreApplicationTests.java
+++ pnuBookStore/src/test/java/org/example/pnubookstore/PnuBookStoreApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.testthymeleaf;
+package org.example.pnubookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestThymeLeafApplicationTests {
+class PnuBookStoreApplicationTests {
 
 	@Test
 	void contextLoads() {
