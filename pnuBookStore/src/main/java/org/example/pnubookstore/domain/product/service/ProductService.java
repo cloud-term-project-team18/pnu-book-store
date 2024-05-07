73cf27008c16fa67206bf4f143eae931acde1353
wndjs803 <wndjs803@naver.com>
2024-05-07 22:49:52 +0900

    feat: product create 구현 - productPicture 추가(임시)

--- pnuBookStore/src/main/java/org/example/pnubookstore/domain/product/service/ProductService.java
+++ pnuBookStore/src/main/java/org/example/pnubookstore/domain/product/service/ProductService.java
@@ -5,8 +5,10 @@ import org.example.pnubookstore.core.exception.Exception404;
 import org.example.pnubookstore.domain.product.dto.CreateProductDto;
 import org.example.pnubookstore.domain.product.ProductExceptionStatus;
 import org.example.pnubookstore.domain.product.entity.Product;
+import org.example.pnubookstore.domain.product.entity.ProductPicture;
 import org.example.pnubookstore.domain.product.entity.Subject;
 import org.example.pnubookstore.domain.product.repository.ProductJpaRepository;
+import org.example.pnubookstore.domain.product.repository.ProductPictureJpaRepository;
 import org.example.pnubookstore.domain.product.repository.SubjectJpaRepository;
 import org.example.pnubookstore.domain.product.repository.UserJpaRepositoryForProduct;
 import org.example.pnubookstore.domain.user.entity.User;
@@ -23,9 +25,12 @@ public class ProductService {
     private final ProductJpaRepository productJpaRepository;
     private final UserJpaRepositoryForProduct userJpaRepositoryForProduct;
     private final SubjectJpaRepository subjectJpaRepository;
+    private final ProductPictureJpaRepository productPictureJpaRepository;
 
     @Transactional
-    public Product createProduct(CreateProductDto createProductDto){
+    public void createProduct(CreateProductDto createProductDto){
+        final String tempUrl = "http://example.com";
+
         User findSeller = userJpaRepositoryForProduct.findUserByEmail(createProductDto.getSellerEmail())
                 .orElseThrow(() -> new Exception404(ProductExceptionStatus.USER_NOT_FOUND.getErrorMessage()));
 
@@ -33,24 +38,29 @@ public class ProductService {
                 createProductDto.getSubjectName(), createProductDto.getDepartment(), createProductDto.getProfessor())
                 .orElseThrow(() -> new Exception404(ProductExceptionStatus.SUBJECT_NOT_FOUND.getErrorMessage()));
 
-        Product product = productJpaRepository.save(Product.builder()
-                .seller(findSeller)
-                .subject(findSubject)
-                .productName(createProductDto.getProductName())
-                .description(createProductDto.getDescription())
-                .author(createProductDto.getAuthor())
-                .pubDate(createProductDto.getPubDate())
-                .isBargain(createProductDto.getIsBargain())
-                .canBargainReason(createProductDto.getCanBargainReason())
-                .saleStatus(createProductDto.getSaleStatus())
-                .underline(createProductDto.getUnderline())
-                .note(createProductDto.getNote())
-                .naming(createProductDto.getNaming())
-                .discolor(createProductDto.getDiscolor())
-                .damage(createProductDto.getDamage())
-                .build());
+        Product createProduct = productJpaRepository.save(
+                Product.builder()
+                    .seller(findSeller)
+                    .subject(findSubject)
+                    .productName(createProductDto.getProductName())
+                    .description(createProductDto.getDescription())
+                    .author(createProductDto.getAuthor())
+                    .pubDate(createProductDto.getPubDate())
+                    .isBargain(createProductDto.getIsBargain())
+                    .canBargainReason(createProductDto.getCanBargainReason())
+                    .saleStatus(createProductDto.getSaleStatus())
+                    .underline(createProductDto.getUnderline())
+                    .note(createProductDto.getNote())
+                    .naming(createProductDto.getNaming())
+                    .discolor(createProductDto.getDiscolor())
+                    .damage(createProductDto.getDamage())
+                    .build());
 
-        return product;
+        productPictureJpaRepository.save(
+                ProductPicture.builder()
+                        .url(tempUrl)
+                        .product(createProduct)
+                        .build());
     }
 
 
