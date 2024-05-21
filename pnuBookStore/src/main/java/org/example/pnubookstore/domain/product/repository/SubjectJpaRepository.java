982a20fd045f9552f5453ea7b4e45691cd8f4a4b
wndjs803 <wndjs803@naver.com>
2024-05-22 00:19:54 +0900

    feat: findColleges, findDepartment 구현

--- pnuBookStore/src/main/java/org/example/pnubookstore/domain/product/repository/SubjectJpaRepository.java
+++ pnuBookStore/src/main/java/org/example/pnubookstore/domain/product/repository/SubjectJpaRepository.java
@@ -2,10 +2,19 @@ package org.example.pnubookstore.domain.product.repository;
 
 import org.example.pnubookstore.domain.product.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubjectJpaRepository extends JpaRepository<Subject, Long> {
     Subject findBySubjectNameAndCollegeAndDepartmentAndProfessor(
             String subjectName, String college, String department, String professor);
+
+    @Query("SELECT s.college FROM Subject s")
+    List<String> findColleges();
+
+    @Query("SELECT s.department FROM Subject s WHERE s.college = :college")
+    List<String> findDepartments(@Param("college") String college);
 }
--- pnuBookStore/src/main/java/org/example/pnubookstore/domain/product/service/SubjectService.java
+++ pnuBookStore/src/main/java/org/example/pnubookstore/domain/product/service/SubjectService.java
@@ -5,10 +5,20 @@ import org.example.pnubookstore.domain.product.repository.SubjectJpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SubjectService {
 
     private final SubjectJpaRepository subjectJpaRepository;
+
+    public List<String> findColleges(){
+        return subjectJpaRepository.findColleges();
+    }
+
+    public List<String> findDepartment(String college){
+        return subjectJpaRepository.findDepartments(college);
+    }
 }
