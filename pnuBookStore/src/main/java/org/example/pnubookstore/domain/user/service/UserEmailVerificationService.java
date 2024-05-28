297d4ab222df2637908640c3d451c71a41a39556
Taeho <rjsdnxogh55@gmail.com>
2024-05-29 02:14:31 +0900

    Feat: Implement Domain Extractor
    - 이메일을 전송하기 위한 Domain Extractor를 구현했습니다.

--- pnuBookStore/src/main/java/org/example/pnubookstore/domain/user/service/UserEmailVerificationService.java
+++ pnuBookStore/src/main/java/org/example/pnubookstore/domain/user/service/UserEmailVerificationService.java
@@ -1,19 +1,17 @@
 package org.example.pnubookstore.domain.user.service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import lombok.RequiredArgsConstructor;
-import org.example.pnubookstore.core.exception.Exception404;
+
 import org.example.pnubookstore.domain.user.entity.EmailVerification;
 import org.example.pnubookstore.domain.user.repository.UserEmailVerificationRedisRepository;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -22,28 +20,27 @@ public class UserEmailVerificationService {
     private final JavaMailSender javaMailSender;
     private final UserEmailVerificationRedisRepository userEmailVerificationRedisRepository;
 
-
+    @Value("${spring.mail.username}")
+    private String from;
 
     public EmailVerification isUserEmailValid(String uuid){
         // 파라미터로 넘겨준 정보를 레디스에서 실제로 있는 유저인지 검색
         return userEmailVerificationRedisRepository.findByUuid(uuid);
     }
 
-    public void sendVerifyEmail(String email, String uuid){
+    public void sendVerifyEmail(String email, String uuid, String domain){
         String subject = "인증 메일";
         String content = "메일 테스트 내용";
-        content += "<a href=\"http://localhost:8080/signUp/after-email?uuid=" + uuid + "\">링크 클릭</a>";
-        String from = "pnuauction@gmail.com";
-        String to = email;
+        content += String.format("<a href=\"%s/signUp/after-email?uuid=\">링크 클릭</a>", domain);
 
-        MimeMessage mail = javaMailSender.createMimeMessage();
+		MimeMessage mail = javaMailSender.createMimeMessage();
         MimeMessageHelper mailHelper = new MimeMessageHelper(mail,"UTF-8");
         try{
-            mailHelper.setFrom(from);
+            mailHelper.setFrom(String.format("부산대학교 중고거래 <%s>", from));
             // 빈에 아이디 설정한 것은 단순히 smtp 인증을 받기 위해 사용 따라서 보내는이(setFrom())반드시 필요
             // 보내는이와 메일주소를 수신하는이가 볼때 모두 표기 되게 원하신다면 아래의 코드를 사용하시면 됩니다.
             //mailHelper.setFrom("보내는이 이름 <보내는이 아이디@도메인주소>");
-            mailHelper.setTo(to);
+            mailHelper.setTo(email);
             mailHelper.setSubject(subject);
             mailHelper.setText(content, true);
             // true는 html을 사용하겠다는 의미입니다.
--- pnuBookStore/src/main/java/org/example/pnubookstore/domain/user/service/UserService.java
+++ pnuBookStore/src/main/java/org/example/pnubookstore/domain/user/service/UserService.java
@@ -3,10 +3,12 @@ package org.example.pnubookstore.domain.user.service;
 import org.example.pnubookstore.domain.user.dto.CreateUserDto;
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Service
 public interface UserService {
 	void createUser(CreateUserDto createUserDto);
-	void emailVerify(String email);
+	void emailVerify(String email, HttpServletRequest request);
 //	void afterEmailForm(CreateUserDto createUserDto);
 
 }
